0f61a75417bd44cecc7721f5db2250569b0ad71c
Aydoğan Sarı <aydogansari@hotmail.com>
2022-03-13 15:56:51 +0300

    Add peerDependencies rollup-plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
@@ -21,6 +22,7 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
@@ -30,6 +32,7 @@ export default [
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     external: ['react', 'react-dom'],
+    global: { react: "React" },
     plugins: [dts()],
   },
 ];
